e66e211bcae8abe250193775b7121471008da124
anurag <anuragch@cybage.com>
2016-05-31 16:12:45 +0530

    first

---
+++ REST-Demo/src/main/java/org/cyb/training/java/rs/model/Student.java
@@ -0,0 +1,29 @@
+package main.java.org.cyb.training.java.rs.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Student {
+
+	String studentName;
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentCourse() {
+		return studentCourse;
+	}
+	public void setStudentCourse(String studentCourse) {
+		this.studentCourse = studentCourse;
+	}
+	int studentId;
+	String studentCourse;
+}
